0591358d444a182471cf9a1b1d3417582e1ee2d5
Chris Meyer <v-chrmey@microsoft.com>
2018-08-16 14:43:45 -0700

    Add viewer service, add interop project, build updates

--- scripts/BuildAndTest.ps1
+++ scripts/BuildAndTest.ps1
@@ -92,7 +92,7 @@ function New-SigningDirectory {
     }
 
     foreach ($project in $Projects.NewProduct) {
-        $projectBinDirectory = (Get-ProjectBinDirectory $project, $configuration)
+        $projectBinDirectory = (Get-ProjectBinDirectory $project $configuration)
 
         foreach ($framework in $Frameworks.All) {
             $sourceDirectory = "$projectBinDirectory\$framework"
