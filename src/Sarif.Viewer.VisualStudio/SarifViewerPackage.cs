d27f83fd05b10f2f61f4f30a62c8346e5d6dbd18
Chris Meyer <v-chrmey@microsoft.com>
2018-09-27 13:34:07 -0700

    Updates for Marketplace & misc.

--- src/Sarif.Viewer.VisualStudio/SarifViewerPackage.cs
+++ src/Sarif.Viewer.VisualStudio/SarifViewerPackage.cs
@@ -7,6 +7,8 @@ using System.Configuration;
 using System.IO;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
 using EnvDTE;
 using EnvDTE80;
 using Microsoft.ApplicationInsights.Extensibility;
@@ -22,14 +24,14 @@ namespace Microsoft.Sarif.Viewer
     /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
     /// </para>
     /// </remarks>
-    [PackageRegistration(UseManagedResourcesOnly = true)]
-    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration("#110", "#112", "2.0 beta", IconResourceID = 400)] // Info on this package for Help/About
     [Guid(SarifViewerPackage.PackageGuidString)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [ProvideEditorExtension(typeof(SarifEditorFactory), ".sarif", 128)]
     [ProvideToolWindow(typeof(SarifToolWindow), Style = VsDockStyle.Tabbed, Window = "3ae79031-e1bc-11d0-8f78-00a0c9110057")]
     [ProvideService(typeof(SLoadSarifLogService))]
-    public sealed class SarifViewerPackage : Package
+    public sealed class SarifViewerPackage : AsyncPackage
     {
         public static DTE2 Dte;
         public static IServiceProvider ServiceProvider;
@@ -95,7 +97,7 @@ namespace Microsoft.Sarif.Viewer
         /// Initialization of the package; this method is called right after the package is sited, so this is the place
         /// where you can put all the initialization code that rely on services provided by VisualStudio.
         /// </summary>
-        protected override void Initialize()
+        protected override Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
             OpenLogFileCommands.Initialize(this);
             base.Initialize();
@@ -127,6 +129,8 @@ namespace Microsoft.Sarif.Viewer
             CodeAnalysisResultManager.Instance.Register();
             SarifToolWindowCommand.Initialize(this);
             ErrorList.ErrorListCommand.Initialize(this);
+
+            return Task.FromResult<object>(null);
         }
 
         #endregion
