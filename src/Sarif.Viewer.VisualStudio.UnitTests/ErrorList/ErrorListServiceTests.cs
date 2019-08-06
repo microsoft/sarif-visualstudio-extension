e7387b9db9537739888ea78e32eded37a11b0eb4
Larry Golding <larrygolding@comcast.net>
2019-08-06 12:34:16 -0700

    Fix #98: Some files won't open.
    In the last commit, the regex that describes the `version` property was broken. Spurious backslash characters were introduced, and the optional whitespace before the colon character (which _is_ permitted by the JSON spec) was removed.
    
    Also:
    - Even though `ErrorListService` as a whole is not unit testable (I filed #100), I extracted a small static method that I could test. This is the Sprout Method (59) pattern from Feathers, "Working Effectively With Legacy Code".
    
    Adding the unit tests revealed another bug: the extension would not have opened a SARIF file that was fewer than 200 characters long.

---
+++ src/Sarif.Viewer.VisualStudio.UnitTests/ErrorList/ErrorListServiceTests.cs
@@ -0,0 +1,83 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using FluentAssertions;
+using Microsoft.Sarif.Viewer.ErrorList;
+using Xunit;
+
+namespace Microsoft.Sarif.Viewer.VisualStudio.UnitTests
+{
+    public class ErrorListServiceTests
+    {
+        private struct TestCase
+        {
+            public string Title { get; set; }
+            public string Input { get; set; }
+            public bool ExpectedMatchSuccess { get; set; }
+            public string ExpectedVersion { get; set; }
+        }
+
+        private static readonly TestCase[] s_testCases = new TestCase[]
+        {
+            new TestCase
+            {
+                Title = "Simplest case",
+                Input = @"""version"": ""2.1.0""",
+                ExpectedMatchSuccess = true,
+                ExpectedVersion = "2.1.0"
+            },
+            new TestCase
+            {
+                Title = "White space before colon",
+                Input = @"""version"" : ""2.1.0""",
+                ExpectedMatchSuccess = true,
+                ExpectedVersion = "2.1.0"
+            },
+            new TestCase
+            {
+                Title = "Version near start of file",
+                Input = @"01234567890123456789 ""version"" : ""2.1.0""",
+                ExpectedMatchSuccess = true,
+                ExpectedVersion = "2.1.0"
+            },
+            new TestCase
+            {
+                Title = "Version not near start of file",
+                Input = @"01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789 ""version"" : ""2.1.0""",
+                ExpectedMatchSuccess = false
+            },
+            new TestCase
+            {
+                Title = "Invalid version",
+                Input = @"""version"": ""a.b.c""",
+                ExpectedMatchSuccess = false
+            }
+        };
+
+        [Fact]
+        [Trait(TestTraits.Bug, "98")]
+        public void ErrorListService_MatchesVersionProperty()
+        {
+            var failedTestCases = new List<TestCase>();
+
+            foreach (TestCase testCase in s_testCases)
+            {
+                Match match = ErrorListService.MatchVersionProperty(testCase.Input);
+
+                bool failed = false;
+                if (match.Success != testCase.ExpectedMatchSuccess) { failed = true; }
+
+                if (!failed && testCase.ExpectedMatchSuccess)
+                {
+                    if (match.Groups["version"].Value != testCase.ExpectedVersion) { failed = true; }
+                }
+
+                if (failed) { failedTestCases.Add(testCase); }
+            }
+
+            failedTestCases.Should().BeEmpty();
+        }
+    }
+}
