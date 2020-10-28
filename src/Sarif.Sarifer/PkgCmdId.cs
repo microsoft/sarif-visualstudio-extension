851c2ac65b3be89d4b641bb9f3969b000b3f2ac0
Larry Golding <lgolding@comcast.net>
2020-10-28 11:02:24 -0700

    Replace copyright notices.

--- src/Sarif.Sarifer/PkgCmdId.cs
+++ src/Sarif.Sarifer/PkgCmdId.cs
@@ -1,14 +1,5 @@
-/***************************************************************************
- 
-Copyright (c) Microsoft Corporation. All rights reserved.
-THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
-ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
-IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
-PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
-
-***************************************************************************/
-
-using System;
+// Copyright (c) Microsoft. All rights reserved. 
+// Licensed under the MIT license. See LICENSE file in the project root for full license information. 
 
 namespace Microsoft.Samples.VisualStudio.MenuCommands
 {
