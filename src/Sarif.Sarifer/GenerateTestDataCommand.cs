2032d0ac590eb84916df4d602a456e3954499744
Larry Golding <lgolding@comcast.net>
2020-11-06 12:53:21 -0800

    Simplify GenerateTestDataCommand

--- src/Sarif.Sarifer/GenerateTestDataCommand.cs
+++ src/Sarif.Sarifer/GenerateTestDataCommand.cs
@@ -5,13 +5,10 @@ using System;
 using System.ComponentModel.Design;
 using System.IO;
 using System.Reflection;
-using System.Threading.Tasks;
 
 using Microsoft.Sarif.Viewer.Interop;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
-
-using Task = System.Threading.Tasks.Task;
 
 namespace Microsoft.CodeAnalysis.Sarif.Sarifer
 {
@@ -26,7 +23,7 @@ namespace Microsoft.CodeAnalysis.Sarif.Sarifer
         {
             this.viewerInterop = new SarifViewerInterop(vsShell);
 
-            MenuCommand menuCommand = new MenuCommand(
+            var menuCommand = new MenuCommand(
                 new EventHandler(this.MenuCommandCallback),
                 new CommandID(Guids.SariferCommandSet, SariferPackageCommandIds.GenerateTestData));
 
@@ -40,18 +37,13 @@ namespace Microsoft.CodeAnalysis.Sarif.Sarifer
         {
             Stream testDataStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(ProofOfConceptResourceName);
 
-            this.SendDataToViewerAsync(testDataStream).FileAndForget(FileAndForget.EventName(SendDataToViewerFailureEventName));
-        }
-
-        private async Task SendDataToViewerAsync(Stream testDataStream)
-        {
             // TODO: Why does this never return true?
             if (!viewerInterop.IsViewerExtensionLoaded)
             {
                 this.viewerInterop.LoadViewerExtension();
             }
 
-            await this.viewerInterop.OpenSarifLogAsync(testDataStream).ConfigureAwait(continueOnCapturedContext: false);
+            this.viewerInterop.OpenSarifLogAsync(testDataStream).FileAndForget(FileAndForget.EventName(SendDataToViewerFailureEventName));
         }
     }
 }
