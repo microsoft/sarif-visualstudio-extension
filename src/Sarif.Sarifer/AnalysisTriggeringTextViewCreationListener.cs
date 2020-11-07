fbe46bdbe31263212af55d2f55a60a5772e88289
Larry Golding <lgolding@comcast.net>
2020-11-06 16:10:07 -0800

    Introduce ITextViewCreationListener to kick off analysis.

---
+++ src/Sarif.Sarifer/AnalysisTriggeringTextViewCreationListener.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft. All rights reserved. 
+// Licensed under the MIT license. See LICENSE file in the project root for full license information. 
+
+using System.ComponentModel.Composition;
+
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace Microsoft.CodeAnalysis.Sarif.Sarifer
+{
+    /// <summary>
+    /// An <see cref="ITextViewCreationListener"/> that triggers a background analyzer that streams
+    /// its results as a SARIF log to the SARIF Viewer extension.
+    /// </summary>
+    [ContentType(AnyContentType)]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    [Export(typeof(ITextViewCreationListener))]
+    public class AnalysisTriggeringTextViewCreationListener : ITextViewCreationListener
+    {
+        private const string AnyContentType = "any";
+
+        /// <inheritdoc/>
+        public void TextViewCreated(ITextView textView)
+        {
+        }
+    }
+}
