6d2b538beecf76ae8bca8fde402b098f7edab7bc
Larry Golding <lgolding@comcast.net>
2020-11-07 11:49:58 -0800

    Finish the plumbing: Open the analysis results in the viewer.

--- src/Sarif.Sarifer/SarifViewerBackgroundAnalysisSink.cs
+++ src/Sarif.Sarifer/SarifViewerBackgroundAnalysisSink.cs
@@ -4,7 +4,13 @@
 using System.ComponentModel.Composition;
 using System.IO;
 
+using Microsoft.Sarif.Viewer.Interop;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
 using Newtonsoft.Json;
+
+using Task = System.Threading.Tasks.Task;
 
 namespace Microsoft.CodeAnalysis.Sarif.Sarifer
 {
@@ -15,11 +21,28 @@ namespace Microsoft.CodeAnalysis.Sarif.Sarifer
     [Export(typeof(IBackgroundAnalysisSink))]
     internal class SarifViewerBackgroundAnalysisSink : IBackgroundAnalysisSink
     {
+        // TODO: LazyCreate sarifViewerInterop.
         /// <inheritdoc/>
         public void Receive(SarifLog log)
         {
+            // TODO: This is TEMPORARY. We write to a file because the PR that provides a stream-
+            // based interop API is not yet approved.
+            // https://github.com/microsoft/sarif-visualstudio-extension/pull/259
             string tempPath = Path.GetTempFileName();
             File.WriteAllText(tempPath, JsonConvert.SerializeObject(log, Formatting.Indented));
+
+            OpenSarifLogAsync(tempPath).FileAndForget(FileAndForgetEventName.SendDataToViewerFailure);
+        }
+
+        private async static Task OpenSarifLogAsync(string tempPath)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            IVsShell shell = Package.GetGlobalService(typeof(SVsShell)) as IVsShell;
+
+            var sarifViewerInterop = new SarifViewerInterop(shell);
+
+            await sarifViewerInterop.OpenSarifLogAsync(tempPath).ConfigureAwait(continueOnCapturedContext: false);
         }
     }
 }
