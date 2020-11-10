d65e473b6005536960902372c84a23383cae7ada
Larry Golding <lgolding@comcast.net>
2020-11-10 10:02:08 -0800

    Rework FileAndForget as in Sarifer.

--- src/Sarif.Sarifer/FileAndForget.cs
+++ src/Sarif.Sarifer/FileAndForget.cs
@@ -1,12 +1,18 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
+
 namespace Microsoft.CodeAnalysis.Sarif.Sarifer
 {
-    internal static class FileAndForget
+    internal static class FileAndForgetEventName
     {
-        private const string FileAndForgetEventNamePrefix = "Microsoft/Sarifer/";
+        private const string Prefix = "Microsoft/Sarifer/";
 
-        internal static string EventName(string suffix) => $"{FileAndForgetEventNamePrefix}{suffix}";
+        private static readonly Lazy<string> s_sendDataToViewerFailure = new Lazy<string>(() => MakeEventName("SendDataToViewer/Failure"));
+
+        internal static string SendDataToViewerFailure => s_sendDataToViewerFailure.Value;
+
+        private static string MakeEventName(string suffix) => $"{Prefix}{suffix}";
     }
 }
---
+++ src/Sarif.Viewer.VisualStudio/FileAndForgetEventName.cs
@@ -0,0 +1,19 @@
+﻿
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace Microsoft.Sarif.Viewer
+{
+    internal static class FileAndForgetEventName
+    {
+        private const string Prefix = "Microsoft/VisualStudioSarifViewer/";
+
+        private static readonly Lazy<string> s_infoBarCloseFailure = new Lazy<string>(() => MakeEventName("InfoBarClose/Failure"));
+
+        internal static string InfoBarCloseFailure => s_infoBarCloseFailure.Value;
+
+        private static string MakeEventName(string suffix) => $"{Prefix}{suffix}";
+    }
+}
