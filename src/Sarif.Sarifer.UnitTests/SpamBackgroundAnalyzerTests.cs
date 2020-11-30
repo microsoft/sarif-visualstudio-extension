a045fe8ed83745a101054d11125faad8f8d32482
Eddy Nakamura <eddynaka@gmail.com>
2020-11-30 14:24:18 -0300

    Rename variables and create consts to be more precise (#297)

--- src/Sarif.Sarifer.UnitTests/SpamBackgroundAnalyzerTests.cs
+++ src/Sarif.Sarifer.UnitTests/SpamBackgroundAnalyzerTests.cs
@@ -21,10 +21,10 @@ namespace Sarif.Sarifer.UnitTests
         [Fact]
         public void LoadPatternFiles_WhenDirectoryDoesNotExist_ShouldReturnEmptyList()
         {
-            var mock = new Mock<IFileSystem>();
-            mock.Setup(fs => fs.DirectoryExists(It.IsAny<string>())).Returns(() => false);
+            var mockFileSystem = new Mock<IFileSystem>();
+            mockFileSystem.Setup(fs => fs.DirectoryExists(It.IsAny<string>())).Returns(false);
 
-            List<SpamRule> rules = SpamBackgroundAnalyzer.LoadPatternFiles(mock.Object, Guid.NewGuid().ToString());
+            List<SpamRule> rules = SpamBackgroundAnalyzer.LoadPatternFiles(mockFileSystem.Object, Guid.NewGuid().ToString());
 
             rules.Should().BeEmpty();
         }
@@ -32,11 +32,15 @@ namespace Sarif.Sarifer.UnitTests
         [Fact]
         public void LoadPatternFiles_WhenDirectoryDoesExistButIsEmpty_ShouldReturnEmptyList()
         {
-            var mock = new Mock<IFileSystem>();
-            mock.Setup(fs => fs.DirectoryExists(It.IsAny<string>())).Returns(() => true);
-            mock.Setup(fs => fs.DirectoryEnumerateFiles(It.IsAny<string>())).Returns(() => new string[] { });
+            const string SpamDirectory = ".spam";
+            const string ProjectDirectory = @"C:\some-project-folder";
 
-            List<SpamRule> rules = SpamBackgroundAnalyzer.LoadPatternFiles(mock.Object, Guid.NewGuid().ToString());
+            var mockFileSystem = new Mock<IFileSystem>();
+            mockFileSystem.Setup(fs => fs.DirectoryExists(It.IsAny<string>())).Returns(false);
+            mockFileSystem.Setup(fs => fs.DirectoryExists(Path.Combine(ProjectDirectory, SpamDirectory))).Returns(true);
+            mockFileSystem.Setup(fs => fs.DirectoryEnumerateFiles(It.IsAny<string>())).Returns(new string[] { });
+
+            List<SpamRule> rules = SpamBackgroundAnalyzer.LoadPatternFiles(mockFileSystem.Object, ProjectDirectory);
 
             rules.Should().BeEmpty();
         }
@@ -44,14 +48,17 @@ namespace Sarif.Sarifer.UnitTests
         [Fact]
         public void LoadPatternFiles_WhenDirectoryDoesExistWithFiles_ShouldReturnRules()
         {
-            const string rulesJson = "[{\"id\" : \"TEST1001\", \"searchPattern\": \"internal class\", \"replacePattern\":\"public class\", \"description\": \"make class public\", \"message\": \"internal class could be public\"}]";
+            const string SpamDirectory = ".spam";
+            const string ProjectDirectory = @"C:\some-project-folder";
+            const string RulesJson = "[{\"id\" : \"TEST1001\", \"searchPattern\": \"internal class\", \"replacePattern\":\"public class\", \"description\": \"make class public\", \"message\": \"internal class could be public\"}]";
 
-            var mock = new Mock<IFileSystem>();
-            mock.Setup(fs => fs.DirectoryExists(It.IsAny<string>())).Returns(() => true);
-            mock.Setup(fs => fs.DirectoryEnumerateFiles(It.IsAny<string>())).Returns(() => new string[] { Guid.NewGuid().ToString() });
-            mock.Setup(fs => fs.FileOpenRead(It.IsAny<string>())).Returns(() => new MemoryStream(Encoding.UTF8.GetBytes(rulesJson)));
+            var mockFileSystem = new Mock<IFileSystem>();
+            mockFileSystem.Setup(fs => fs.DirectoryExists(It.IsAny<string>())).Returns(false);
+            mockFileSystem.Setup(fs => fs.DirectoryExists(Path.Combine(ProjectDirectory, SpamDirectory))).Returns(true);
+            mockFileSystem.Setup(fs => fs.DirectoryEnumerateFiles(It.IsAny<string>())).Returns(new string[] { Guid.NewGuid().ToString() });
+            mockFileSystem.Setup(fs => fs.FileOpenRead(It.IsAny<string>())).Returns(new MemoryStream(Encoding.UTF8.GetBytes(RulesJson)));
 
-            List<SpamRule> rules = SpamBackgroundAnalyzer.LoadPatternFiles(mock.Object, Guid.NewGuid().ToString());
+            List<SpamRule> rules = SpamBackgroundAnalyzer.LoadPatternFiles(mockFileSystem.Object, ProjectDirectory);
 
             rules.Should().HaveCount(1);
             rules[0].Id.Should().Be("TEST1001");
