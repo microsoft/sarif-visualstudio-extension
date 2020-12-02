5d119b7a4a9cd0484ef17c705b12b4a7696001a5
Eddy Nakamura <ednakamu@microsoft.com>
2020-12-02 19:37:19 -0300

    adding reporoot variable

--- scripts/BuildAndTest.ps1
+++ scripts/BuildAndTest.ps1
@@ -144,7 +144,7 @@ if (-not $NoRestore) {
 }
 
 if (-not $NoBuild) {
-    .\src\sarif-pattern-matcher\BuildAndTest.cmd
+    $RepoRoot\src\sarif-pattern-matcher\BuildAndTest.cmd
     Invoke-Build
 }
 
