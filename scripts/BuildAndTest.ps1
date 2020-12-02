1d62b948387b2d7390e0c20f30098b4717065bb5
Eddy Nakamura <ednakamu@microsoft.com>
2020-12-02 20:08:36 -0300

    updating script

--- scripts/BuildAndTest.ps1
+++ scripts/BuildAndTest.ps1
@@ -144,7 +144,8 @@ if (-not $NoRestore) {
 }
 
 if (-not $NoBuild) {
-    & $RepoRoot\src\sarif-pattern-matcher\BuildAndTest.cmd
+    & $RepoRoot\src\sarif-pattern-matcher\BuildAndTest.cmd -NoTest
+    & $RepoRoot\src\sarif-sdk\BuildAndTest.cmd -NoTest
     if ($LASTEXITCODE -ne 0) {
         Exit-WithFailureMessage $ScriptName "sarif-pattern-matcher failed."
     }
