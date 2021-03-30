023894b0b61f6c4b0e3f4adcdfd18fe8ef62b7a8
Yong Yan <yanyong-gh@outlook.com>
2021-03-30 13:20:10 -0700

    Fix null reference exception issue when analyze solution (#387)
    * Fix null reference exception issue when analyze solution
    
    * Fix typo
    
    * Handle other scenario virtual project folder and virtual solution folder has projects
    
    * Fix typo
    
    * Update sarif-pattern-matcher submodule to latest

--- src/Sarif.Sarifer/Commands/SariferPackageCommand.cs
+++ src/Sarif.Sarifer/Commands/SariferPackageCommand.cs
@@ -58,9 +58,24 @@ namespace Microsoft.CodeAnalysis.Sarif.Sarifer.Commands
                     GetFilesRecursive(targetFiles, projectFolder);
                 }
             }
+            else if (projectItem.Kind == Constants.vsProjectItemKindSolutionItems && projectItem.SubProject != null)
+            {
+                // this is for case solution has a folder at solution level and looking to its sub project
+                targetFiles.AddRange(GetFiles(projectItem.SubProject));
+            }
+            else if (projectItem.Kind == Constants.vsProjectItemKindVirtualFolder)
+            {
+                // this is for case a project has a virtual folder which contains a bunch of physical files
+                foreach (ProjectItem projectFolder in projectItem.ProjectItems)
+                {
+                    GetFilesRecursive(targetFiles, projectFolder);
+                }
+            }
             else
             {
-                string localPath = projectItem.Properties.Item("LocalPath").Value.ToString();
+                // this is case of a physical file
+                // handle some solution items has null property. e.g. Solution Items, avoid null reference exception
+                string localPath = projectItem.Properties?.Item("FullPath").Value.ToString();
                 if (!string.IsNullOrWhiteSpace(localPath))
                 {
                     targetFiles.Add(localPath);
