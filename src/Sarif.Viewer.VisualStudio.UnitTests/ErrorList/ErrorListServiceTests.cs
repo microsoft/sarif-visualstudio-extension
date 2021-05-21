05bbf1260f698830566b4f9b93069e444c26944f
Yong Yan <yanyong-gh@outlook.com>
2021-05-21 14:35:25 -0700

    Handle Json deserialize error

--- src/Sarif.Viewer.VisualStudio.UnitTests/ErrorList/ErrorListServiceTests.cs
+++ src/Sarif.Viewer.VisualStudio.UnitTests/ErrorList/ErrorListServiceTests.cs
@@ -2,6 +2,8 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using System.Text.RegularExpressions;
 
 using FluentAssertions;
@@ -75,6 +77,80 @@ namespace Microsoft.Sarif.Viewer.VisualStudio.UnitTests
             failedTestCases.Should().BeEmpty();
         }
 
+        [Fact]
+        public void ErrorListService_ProcessSarifLogAsync_InvalidJson()
+        {
+            // unhook original event handler and hook test event
+            ErrorListService.LogProcessed -= ErrorListService.ErrorListService_LogProcessed;
+            ErrorListService.LogProcessed += ErrorListServiceTest_LogProcessed;
+
+            // invalid Json syntax, a separate comma in line 4
+            // {"Invalid property identifier character: ,. Path '$schema', line 4, position 2."}
+            string invalidJson =
+@"
+{
+  ""$schema"": ""https://schemastore.azurewebsites.net/schemas/json/sarif-2.1.0-rtm.4.json"",
+  ,
+  ""version"": ""2.1.0"",
+  ""runs"": [
+    {
+      ""tool"": {
+        ""name"": ""CodeScanner""
+      },
+      ""results"": [
+      ]
+    }
+  ]
+}
+";
+            int numberOfException = numberOfExceptionLogged;
+            using var stream = new MemoryStream(Encoding.UTF8.GetBytes(invalidJson));
+            ErrorListService.ProcessSarifLogAsync(stream, "logId", false, false).ConfigureAwait(false);
+            // 1 exception logged
+            numberOfExceptionLogged.Should().Be(numberOfException + 1);
+        }
+
+        [Fact]
+        public void ErrorListService_ProcessSarifLogAsync_JsonNotCompatibleWithSarifShema()
+        {
+            // unhook original event handler and hook test event
+            ErrorListService.LogProcessed -= ErrorListService.ErrorListService_LogProcessed;
+            ErrorListService.LogProcessed += ErrorListServiceTest_LogProcessed;
+
+            // valid Json syntax, but not satisfy schema, missing required property driver
+            // {"Required property 'text' not found in JSON. Path 'runs[0].tool.driver.rules[0].fullDescription', line 14, position 36."}
+            string jsonNotCompatible =
+@"
+{
+  ""$schema"": ""https://schemastore.azurewebsites.net/schemas/json/sarif-2.1.0-rtm.4.json"",
+  ""version"": ""2.1.0"",
+  ""runs"": [
+    {
+      ""tool"": {
+        ""driver"": {
+          ""name"": ""CodeScanner"",
+          ""rules"": [
+            {
+              ""id"": ""Intrafile1001"",
+              ""name"": ""IntrafileRule"",
+              ""fullDescription"": { },
+              ""helpUri"": ""https://github.com/microsoft/sarif-pattern-matcher""
+            }
+          ]
+        },
+      ""results"": [
+      ]
+    }
+  ]
+}
+";
+            int numberOfException = this.numberOfExceptionLogged;
+            using var stream = new MemoryStream(Encoding.UTF8.GetBytes(jsonNotCompatible));
+            ErrorListService.ProcessSarifLogAsync(stream, "logId", false, false).ConfigureAwait(false);
+            // 1 exception logged
+            this.numberOfExceptionLogged.Should().Be(numberOfException + 1);
+        }
+
         private struct TestCase
         {
             public string Title { get; set; }
@@ -84,6 +160,12 @@ namespace Microsoft.Sarif.Viewer.VisualStudio.UnitTests
             public bool ExpectedMatchSuccess { get; set; }
 
             public string ExpectedVersion { get; set; }
+        }
+
+        private int numberOfExceptionLogged = 0;
+        private void ErrorListServiceTest_LogProcessed(object sender, LogProcessedEventArgs e)
+        {
+            this.numberOfExceptionLogged++;
         }
     }
 }
