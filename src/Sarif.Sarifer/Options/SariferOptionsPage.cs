44e906873fb8cfd5384f35b1ab80f7b79862dbe8
Yong Yan <yanyong-gh@outlook.com>
2021-05-20 18:37:35 -0700

    Refactor vs extension options Add option to allow background analyser generate Pass result

---
+++ src/Sarif.Sarifer/Options/SariferOptionsPage.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+using Microsoft.VisualStudio.Shell;
+
+namespace Microsoft.CodeAnalysis.Sarif.Sarifer
+{
+    [Guid("BB3665D5-E661-48C0-801A-19B034F3CD5F")]
+    [ComVisible(true)]
+    public class SariferOptionsPage : UIElementDialogPage
+    {
+        private readonly Lazy<SariferOptionsControl> _sariferOptionsControl;
+
+        public SariferOptionsPage()
+        {
+            _sariferOptionsControl = new Lazy<SariferOptionsControl>(() => new SariferOptionsControl(this));
+        }
+
+        public bool BackgroundAnalysisEnabled { get; set; } = true;
+
+        public bool AnalyzeSarifFile { get; set; } = false;
+
+        public bool IncludesPassResults { get; set; } = false;
+
+        /// <summary>
+        /// Gets the Windows Presentation Foundation (WPF) child element to be hosted inside the Options dialog page.
+        /// </summary>
+        /// <returns>The WPF child element.</returns>
+        protected override UIElement Child
+        {
+            get
+            {
+                return _sariferOptionsControl.Value;
+            }
+        }
+
+        public override void ResetSettings()
+        {
+            base.ResetSettings();
+        }
+
+        /// <summary>
+        /// This occurs when the User selecting 'Ok' and right before the dialog page UI closes entirely.
+        /// This override handles the case when the user types inside an editable combobox and
+        /// immediately hits enter causing the window to close without firing the combobox LostFocus event.
+        /// </summary>
+        /// <param name="e">Event arguments.</param>
+        protected override void OnApply(PageApplyEventArgs e)
+        {
+            base.OnApply(e); // Saves the user's changes.
+        }
+
+        /// <summary>
+        /// This page is called when VS wants to activate this page.
+        /// ie. when the user opens the tools options page.
+        /// </summary>
+        /// <param name="e">Cancellation event arguments.</param>
+        protected override void OnActivate(CancelEventArgs e)
+        {
+            // The UI caches the settings even though the tools options page is closed.
+            // This load call ensures we display data that was saved. This is to handle
+            // the case when the user hits the cancel button and reloads the page.
+            LoadSettingsFromStorage();
+
+            base.OnActivate(e);
+        }
+    }
+}
