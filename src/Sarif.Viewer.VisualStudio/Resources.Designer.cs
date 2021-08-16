f886a49448dd072c079ca451be64b7dfdcc22260
Eddy Nakamura <ednakamu@microsoft.com>
2021-08-16 08:53:50 -0300

    Fixing test adding clangtidy extension type

--- src/Sarif.Viewer.VisualStudio/Resources.Designer.cs
+++ src/Sarif.Viewer.VisualStudio/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Microsoft.Sarif.Viewer {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -320,6 +320,15 @@ namespace Microsoft.Sarif.Viewer {
         public static string ImportClangAnalyzerFilter {
             get {
                 return ResourceManager.GetString("ImportClangAnalyzerFilter", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Clang log files (*.yaml)|*.yaml.
+        /// </summary>
+        public static string ImportClangTidyFilter {
+            get {
+                return ResourceManager.GetString("ImportClangTidyFilter", resourceCulture);
             }
         }
         
