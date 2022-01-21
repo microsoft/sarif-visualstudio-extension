0fb3e6870a174c37c2946ad32158f1bdd080942d
Yong Yan <yanyong-gh@outlook.com>
2022-01-21 01:00:56 -0800

    Fix CodeFlows/ThreadFlows rendering issue (#452)
    * Update rendering for codeflows
    
    * update sub module
    
    * Set indent using text margin instead of adding indent in text
    
    * address review issues

--- src/Sarif.Viewer.VisualStudio.UnitTests/Models/AnalysisStepNodeTests.cs
+++ src/Sarif.Viewer.VisualStudio.UnitTests/Models/AnalysisStepNodeTests.cs
@@ -1,6 +1,9 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
+using System.Windows;
+
 using FluentAssertions;
 
 using Microsoft.CodeAnalysis.Sarif;
@@ -11,6 +14,8 @@ namespace Microsoft.Sarif.Viewer.Models
 {
     public class AnalysisStepNodeTests
     {
+        private static readonly Random s_random = new Random();
+
         [Fact]
         public void AnalysisStepNode_DefaultHighlightColor()
         {
@@ -39,6 +44,25 @@ namespace Microsoft.Sarif.Viewer.Models
 
             analysisStepNode.Location.Importance = ThreadFlowLocationImportance.Essential;
             analysisStepNode.SelectedSourceHighlightColor.Should().Be("CodeAnalysisCurrentStatementSelection");
+        }
+
+        [Fact]
+        public void AnalysisStepNode_TextMargin()
+        {
+            var analysisStepNode = new AnalysisStepNode(resultId: 0, runIndex: 0);
+
+            analysisStepNode.TextMargin.Should().NotBeNull();
+            analysisStepNode.TextMargin.Left.Should().Be(0);
+            analysisStepNode.TextMargin.Right.Should().Be(0);
+            analysisStepNode.TextMargin.Top.Should().Be(0);
+            analysisStepNode.TextMargin.Bottom.Should().Be(0);
+
+            analysisStepNode.NestingLevel = s_random.Next(0, 30);
+
+            analysisStepNode.TextMargin.Left.Should().Be(AnalysisStepNode.IndentWidth * analysisStepNode.NestingLevel);
+            analysisStepNode.TextMargin.Right.Should().Be(0);
+            analysisStepNode.TextMargin.Top.Should().Be(0);
+            analysisStepNode.TextMargin.Bottom.Should().Be(0);
         }
     }
 }
