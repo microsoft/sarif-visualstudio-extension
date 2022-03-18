78e340aa687125d3d9f233ea65d87ddfa887012b
Chris Meyer <cmeyer@microsoft.com>
2022-03-18 06:43:36 -1000

    Test library project

---
+++ src/ClassLibrary1/Class1.cs
@@ -0,0 +1,12 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public Maybe<string> Test()
+        {
+            return default(Maybe<string>);
+        }
+    }
+}
