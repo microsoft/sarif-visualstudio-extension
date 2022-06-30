87a77dd4dd8783046aaf9f8674d34f59cad683d0
Chris Meyer <cmeyer@microsoft.com>
2022-06-30 10:31:38 -1000

    Add services w/stub method + interop surface

---
+++ src/Sarif.Viewer.VisualStudio.Core/Services/DataService.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.CodeAnalysis.Sarif;
+
+namespace Microsoft.Sarif.Viewer.Services
+{
+    /// <inheritdoc/>
+    public class DataService : SDataService, IDataService
+    {
+        /// <inheritdoc/>
+        public void SendEnhancedResultData(SarifLog sarifLog)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/Sarif.Viewer.VisualStudio.Core/Services/IDataService.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.CodeAnalysis.Sarif;
+
+namespace Microsoft.Sarif.Viewer.Services
+{
+    /// <summary>
+    /// Provides methods for sending data to the SARIF Viewer extension.
+    /// </summary>
+    internal interface IDataService
+    {
+        /// <summary>
+        /// Sends enhanced SARIF result data.
+        /// </summary>
+        /// <param name="sarifLog">A <see cref="SarifLog"/> containing the enhanced result data.</param>
+        void SendEnhancedResultData(SarifLog sarifLog);
+    }
+}
