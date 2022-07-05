b436dca820448db41497c0f53058b6bfc4b47a53
Chris Meyer <cmeyer@microsoft.com>
2022-07-05 10:05:35 -1000

    Update Projects.psm1

--- scripts/Projects.psm1
+++ scripts/Projects.psm1
@@ -18,17 +18,21 @@ $Projects.Vsix = @(
 	"Sarif.Sarifer")
 $Projects.NuGet = @("Sarif.Viewer.VisualStudio.Interop")
 $Projects.Library = @(
+	"Sarif.Viewer.VisualStudio.ResultSources.ACL.2022",
 	"Sarif.Viewer.VisualStudio.ResultSources.ACL",
 	"Sarif.Viewer.VisualStudio.ResultSources.Domain.2022",
 	"Sarif.Viewer.VisualStudio.ResultSources.Domain",
 	"Sarif.Viewer.VisualStudio.Shell.2022",
 	"Sarif.Viewer.VisualStudio.Shell",
+	"Sarif.Viewer.VisualStudio.ResultSources.Factory.2022",
 	"Sarif.Viewer.VisualStudio.ResultSources.Factory",
 	"Sarif.Viewer.VisualStudio.ResultSources.GitHubAdvancedSecurity.2022",
 	"Sarif.Viewer.VisualStudio.ResultSources.GitHubAdvancedSecurity")
 $Projects.Product = $Projects.Vsix + $Projects.NuGet + $Projects.Library
 $Projects.Test = @(
 	"Sarif.Viewer.VisualStudio.UnitTests",
+	"Sarif.Viewer.VisualStudio.ResultSources.Factory.UnitTests",
+	"Sarif.Viewer.VisualStudio.ResultSources.GitHubAdvancedSecurity.UnitTests",
 	"Sarif.Sarifer.UnitTests")
 $Projects.All = $Projects.Product + $Projects.Test
 
