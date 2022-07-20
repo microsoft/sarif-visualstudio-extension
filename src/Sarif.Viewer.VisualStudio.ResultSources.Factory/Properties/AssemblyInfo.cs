34746643d0768b7e529350bbf55ea16047a9c712
Chris Meyer <cmeyer@microsoft.com>
2022-07-20 13:03:32 -1000

    Harden result source handling (#506)
    * Handle solutions that aren't in a repo
    
    * Improve RequestAnalysisResultsAsync + add unit test
    
    * PR feedback
    
    * PR feedback
    
    I'm reluctant to swallow Exception like this but for now it's OK. I'll file a bug if I think of a better way.

--- src/Sarif.Viewer.VisualStudio.ResultSources.Factory/Properties/AssemblyInfo.cs
+++ src/Sarif.Viewer.VisualStudio.ResultSources.Factory/Properties/AssemblyInfo.cs
@@ -1,7 +1,10 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0298e0ab-0f86-433f-9423-5db950809f62")]
+
+[assembly: InternalsVisibleTo("Sarif.Viewer.VisualStudio.ResultSources.Factory.UnitTests")]
