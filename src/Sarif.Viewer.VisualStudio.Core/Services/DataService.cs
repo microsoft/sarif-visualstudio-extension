230c6f413709d841d6ee80ee8045ac3c0987141b
Chris Meyer <cmeyer@microsoft.com>
2022-07-20 14:08:21 -1000

    Pull 'users/cmeyer/key events interop' into 'feature/key events' (#508)
    * Add services w/stub method + interop surface
    
    * Use SARIF SDK project ref instead of NuGet package
    
    * WIP
    
    * Plumbing to load enhanced data + cleanup
    
    * Cleanup
    
    * Cleanup

---
+++ src/Sarif.Viewer.VisualStudio.Core/Services/DataService.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using EnvDTE;
+
+using EnvDTE80;
+
+using Microsoft.CodeAnalysis.Sarif;
+using Microsoft.Sarif.Viewer.ErrorList;
+using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.Shell;
+
+using Task = System.Threading.Tasks.Task;
+
+namespace Microsoft.Sarif.Viewer.Services
+{
+    /// <inheritdoc/>
+    public class DataService : SDataService, IDataService
+    {
+        /// <inheritdoc/>
+        public void SendEnhancedResultData(SarifLog sarifLog)
+        {
+            SendEnhancedResultDataAsync(sarifLog).FileAndForget(Constants.FileAndForgetFaultEventNames.SendEnhancedData);
+        }
+
+        private static async Task SendEnhancedResultDataAsync(SarifLog sarifLog)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            Assumes.NotNull(sarifLog);
+            Assumes.True(sarifLog.Runs?.Count == 1);
+
+            var componentModel = (IComponentModel)AsyncPackage.GetGlobalService(typeof(SComponentModel));
+            if (componentModel != null)
+            {
+                var dte = Package.GetGlobalService(typeof(DTE)) as DTE2;
+                var projectNameCache = new ProjectNameCache(dte?.Solution);
+
+                Result result = sarifLog.Runs.First().Results.First();
+                var sarifErrorListItem = new SarifErrorListItem(sarifLog.Runs.First(), 0, result, string.Empty, projectNameCache);
+                sarifErrorListItem.PopulateAdditionalPropertiesIfNot();
+
+                ISarifErrorListEventSelectionService sarifErrorListEventSelectionService = componentModel.GetService<ISarifErrorListEventSelectionService>();
+                sarifErrorListEventSelectionService.NavigatedItem = sarifErrorListItem;
+
+                sarifErrorListItem.Locations?.FirstOrDefault()?.NavigateTo(usePreviewPane: false, moveFocusToCaretLocation: true);
+            }
+
+            SarifExplorerWindow.Find()?.Show();
+        }
+    }
+}
