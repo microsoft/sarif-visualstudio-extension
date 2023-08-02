ebe2df8d6be1b1943b85982e627ab713d04f47f1
Edward Carlson <ecarlson@microsoft.com>
2023-08-02 14:20:33 -0700

    added ui for menu

--- src/Sarif.Viewer.VisualStudio.Core/Options/GeneralOptions/SarifViewerGeneralOptionsPage.cs
+++ src/Sarif.Viewer.VisualStudio.Core/Options/GeneralOptions/SarifViewerGeneralOptionsPage.cs
@@ -26,6 +26,8 @@ namespace Microsoft.Sarif.Viewer.Options
 
         public bool EnableKeyEventAdornment { get; set; } = true;
 
+        public int SelectedSlot { get; set; } = 0;
+
         /// <summary>
         /// Gets the Windows Presentation Foundation (WPF) child element to be hosted inside the Options dialog page.
         /// </summary>
